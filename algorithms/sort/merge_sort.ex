86f28643a8a6a78fe24b432c77f0d43d70599eb0
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2023-12-13 16:07:28 -0300

    Implementing merge sort

--- algorithms/sort/merge_sort.ex
+++ algorithms/sort/merge_sort.ex
@@ -1,9 +1,17 @@
 defmodule Sort.MergeSort do
-
-  def merge([h1|t1], [h2|t2]) do
+  defp merge([], []), do: []
+  defp merge(l, []), do: l
+  defp merge([], l), do: l
+  #defpmerge([h1|t1], [h2|t2]) when h1 < h2 do
+  #  [h1 | merge(t1, [h2|t2])]
+  #end
+  #defpmerge([h1|t1], [h2|t2]) when h1 >= h2 do
+  #  [h2 | merge([h1|t1], t2)]
+  #end
+  defp merge([h1|t1], [h2|t2]) do
     cond do
-      h1 < h2 -> [h1|t1] ++ [h2|t2]
-      true    -> [h2|t2] ++ [h1|t1]
+      h1 < h2 -> [h1 | merge(t1, [h2|t2])]
+      true    -> [h2 | merge([h1|t1], t2)]
     end
   end
 
@@ -11,6 +19,7 @@ defmodule Sort.MergeSort do
   def sort([i]), do: [i]
   def sort(list) do
     n = round(length(list) / 2)
-
-
+    {left, right} = Enum.split(list, n)
+    merge(sort(left), sort(right))
+  end
 end
